86f91ab7073f39bcd180c59d3b628c4e2dd224bd
tsai1247 <jeff29cc91079@gmail.com>
2023-09-13 15:59:00 +0800

    [新增] 刪除操作的欄位

--- main.py
+++ main.py
@@ -14,10 +14,11 @@ root = tk.Tk()
 root.title("3*n 列表")
 
 # 創建表格
-tree = ttk.Treeview(root, columns=("Column 1", "Column 2", "Column 3"), show="headings")
+tree = ttk.Treeview(root, columns=("Column 1", "Column 2", "Column 3", "操作"), show="headings")
 tree.heading("Column 1", text="Column 1")
 tree.heading("Column 2", text="Column 2")
 tree.heading("Column 3", text="Column 3")
+tree.heading("操作", text="操作")
 tree.pack()
 
 # 創建輸入框
@@ -38,19 +39,26 @@ entry3.pack()
 
 # 初始化表格中的數據
 for row_data in data:
-    tree.insert('', 'end', values=row_data)
+    tree.insert('', 'end', values=row_data + ("刪除",))
 
 # 添加行的函數
 def add_row():
-    values = [entry1.get(), entry2.get(), entry3.get()]
+    values = (entry1.get(), entry2.get(), entry3.get(), "刪除")
     tree.insert('', 'end', values=values)
     save_to_json()
+
+# 刪除行的函數
+def delete_row():
+    selected_item = tree.selection()
+    if selected_item:
+        tree.delete(selected_item)
+        save_to_json()
 
 # 將新增的內容保存到 JSON 文件中
 def save_to_json():
     data = []
     for item in tree.get_children():
-        values = tree.item(item, 'values')
+        values = tree.item(item, 'values')[:-1]  # 去掉最後一個 "刪除"
         if values:
             data.append(values)
     
@@ -60,6 +68,10 @@ def save_to_json():
 # 添加行的按鈕
 add_button = tk.Button(root, text="添加行", command=add_row)
 add_button.pack()
+
+# 刪除行的按鈕
+delete_button = tk.Button(root, text="刪除選定行", command=delete_row)
+delete_button.pack()
 
 # 啟動應用程序的主迴圈
 root.mainloop()
