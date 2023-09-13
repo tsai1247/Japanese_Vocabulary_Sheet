86cf1a7e5b457c8bc6bdb1d7c779eed8b681cda6
tsai1247 <jeff29cc91079@gmail.com>
2023-09-13 16:15:23 +0800

    [修改] 調整版面字體與顏色

--- main.py
+++ main.py
@@ -18,49 +18,63 @@ except FileNotFoundError:
 root = tk.Tk()
 root.title("3*n 列表")
 
-# 創建表格
+# 设置全局字体大小
+font_style = ttk.Style()
+font_style.configure("Treeview", font=("Arial", 14))  # 设置字体大小
+
+# 创建表格样式
+tree_style = ttk.Style()
+tree_style.configure("Treeview", background="white", foreground="black")  # 设置表格背景和前景颜色
+tree_style.configure("Treeview.Heading", font=("Arial", 14, "bold"))  # 设置表头的字体和样式
+tree_style.map("Treeview", background=[("selected", "#A3A3A3")])  # 设置选中行的背景颜色
+
+# 设置表头颜色
 tree = ttk.Treeview(root, columns=(COLUMN_1, COLUMN_2, COLUMN_3), show="headings")
-tree.heading(COLUMN_1, text=COLUMN_1)
-tree.heading(COLUMN_2, text=COLUMN_2)
-tree.heading(COLUMN_3, text=COLUMN_3)
+tree.heading(COLUMN_1, text=COLUMN_1, anchor="center")
+tree.heading(COLUMN_2, text=COLUMN_2, anchor="center")
+tree.heading(COLUMN_3, text=COLUMN_3, anchor="center")
 tree.pack()
 
-# 創建一個框架來容納輸入框和添加按鈕
+# 创建一个框架来容纳输入框和添加按钮
 input_frame = tk.Frame(root)
-input_frame.pack(pady=10)  # 增加垂直間距
+input_frame.pack(pady=10)  # 增加垂直间距
 
-# 創建輸入框
-label1 = tk.Label(input_frame, text=f"{COLUMN_1}:")
-label1.grid(row=0, column=0, padx=5)  # 增加水平間距
-entry1 = tk.Entry(input_frame)
-entry1.grid(row=0, column=1, padx=5)  # 增加水平間距
+# 创建输入框样式
+entry_style = ttk.Style()
+entry_style.configure("Entry.TEntry", font=("Arial", 14))  # 设置输入框的字体大小
 
-label2 = tk.Label(input_frame, text=f"{COLUMN_2}:")
-label2.grid(row=0, column=2, padx=5)  # 增加水平間距
-entry2 = tk.Entry(input_frame)
-entry2.grid(row=0, column=3, padx=5)  # 增加水平間距
+# 创建输入框
+label1 = tk.Label(input_frame, text=f"{COLUMN_1}:", font=("Arial", 14))
+label1.grid(row=0, column=0, padx=5)  # 增加水平间距
+entry1 = ttk.Entry(input_frame, font=("Arial", 14), style="Entry.TEntry")
+entry1.grid(row=0, column=1, padx=5)  # 增加水平间距
 
-label3 = tk.Label(input_frame, text=f"{COLUMN_3}:")
-label3.grid(row=0, column=4, padx=5)  # 增加水平間距
-entry3 = tk.Entry(input_frame)
-entry3.grid(row=0, column=5, padx=5)  # 增加水平間距
+label2 = tk.Label(input_frame, text=f"{COLUMN_2}:", font=("Arial", 14))
+label2.grid(row=0, column=2, padx=5)  # 增加水平间距
+entry2 = ttk.Entry(input_frame, font=("Arial", 14), style="Entry.TEntry")
+entry2.grid(row=0, column=3, padx=5)  # 增加水平间距
 
-# 添加行的函數
+label3 = tk.Label(input_frame, text=f"{COLUMN_3}:", font=("Arial", 14))
+label3.grid(row=0, column=4, padx=5)  # 增加水平间距
+entry3 = ttk.Entry(input_frame, font=("Arial", 14), style="Entry.TEntry")
+entry3.grid(row=0, column=5, padx=5)  # 增加水平间距
+
+# 添加行的函数
 def add_row():
     value1 = entry1.get().strip()
     value2 = entry2.get().strip()
     value3 = entry3.get().strip()
     
-    if value1 and value2 and value3:  # 檢查是否都有輸入值
+    if value1 and value2 and value3:  # 检查是否都有输入值
         values = [value1, value2, value3]
         tree.insert('', 'end', values=values)
         save_to_json()
-        # 清除輸入框的文字
+        # 清除输入框的文字
         entry1.delete(0, 'end')
         entry2.delete(0, 'end')
         entry3.delete(0, 'end')
 
-# 刪除行的函數
+# 删除行的函数
 def delete_row():
     selected_items = tree.selection()
     if selected_items:
@@ -68,7 +82,7 @@ def delete_row():
             tree.delete(item)
         save_to_json()
 
-# 將新增的內容保存到 JSON 文件中
+# 将新增的内容保存到 JSON 文件中
 def save_to_json():
     data = []
     for item in tree.get_children():
@@ -79,22 +93,22 @@ def save_to_json():
     with open("data.json", "w", encoding="utf-8") as json_file:
         json.dump(data, json_file, ensure_ascii=False, indent=4)
 
-# 添加行的按鈕
-add_button = tk.Button(input_frame, text="添加行", command=add_row)
-add_button.grid(row=0, column=6, padx=5)  # 增加水平間距
+# 添加行的按钮
+add_button = tk.Button(input_frame, text="添加行", command=add_row, font=("Arial", 14))
+add_button.grid(row=0, column=6, padx=5)  # 增加水平间距
 
-# 刪除行的按鈕
-delete_button = tk.Button(input_frame, text="刪除選定行", command=delete_row)
-delete_button.grid(row=0, column=7, padx=5)  # 增加水平間距
+# 删除行的按钮
+delete_button = tk.Button(input_frame, text="删除选定行", command=delete_row, font=("Arial", 14))
+delete_button.grid(row=0, column=7, padx=5)  # 增加水平间距
 
 # 绑定 Enter 键的事件处理程序以保存数据
 entry1.bind("<Return>", lambda event=None: add_row())
 entry2.bind("<Return>", lambda event=None: add_row())
 entry3.bind("<Return>", lambda event=None: add_row())
 
-# 初始化表格中的數據
-for row_data in data:
+# 初始化表格中的数据
+for i, row_data in enumerate(data, start=1):
     tree.insert('', 'end', values=row_data)
 
-# 啟動應用程序的主迴圈
+# 启动应用程序的主循环
 root.mainloop()
